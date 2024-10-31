a308a036cafc87404d72dd406f4be5c76f841fa6
DeepFriedChicken-1 <k2315735@students.katyisd.org>
2024-10-31 07:59:46 -0500

    Switched to gunicorn

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request
 import praw
 import random
@@ -40,4 +41,5 @@ def meme():
     return render_template('index.html', meme_image_url=None)
 
 if __name__ == '__main__':
-    app.run( host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 for local dev
+    app.run(host="0.0.0.0", port=port)
